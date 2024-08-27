32751a8b087fdcfbb2ae7e6f2eee434dccb25d1e
vibhamasti <vsmasti@gmail.com>
2024-08-27 16:15:04 -0400

    Added --scheduler

--- run_finetune_expanded.sh
+++ run_finetune_expanded.sh
@@ -1,9 +1,21 @@
 #!/bin/bash
+#SBATCH --mem=32G
+#SBATCH --gres=gpu:A6000:1
+#SBATCH --output=.slurm_logs/fine_tune_expanded_small_schedule.out
+#SBATCH --time=01-00:00
+#SBATCH --mail-type=ALL
+#SBATCH --mail-user=vmasti@andrew.cmu.edu
+
+# combinations=(
+#     "1.4b 2.8b"
+#     "1.4b 6.9b"
+#     "2.8b 6.9b"
+# )
 
 combinations=(
-    "1.4b 2.8b"
-    "1.4b 6.9b"
-    "2.8b 6.9b"
+    "70m 160m"
+    "70m 410m"
+    "160m 410m"
 )
 
 # Loop through each combination
@@ -11,5 +23,5 @@ for combo in "${combinations[@]}"; do
     # Split the combination into small_model and large_model
     read -r small large <<< "$combo"
     echo "Running experiment with small_model=$small and large_model=$large"
-    accelerate launch instruction_tuning_expanded.py --small_model $small --large_model $large
+    accelerate launch instruction_tuning_expanded.py --small_model $small --large_model $large --expand_method padding --scheduler
 done
--- run_finetune_raw.sh
+++ run_finetune_raw.sh
@@ -1,11 +1,18 @@
 #!/bin/bash
+#SBATCH --mem=32G
+#SBATCH --gres=gpu:A6000:1
+#SBATCH --output=.slurm_logs/fine_tune_raw_small.out
+#SBATCH --time=01-00:00
+#SBATCH --mail-type=ALL
+#SBATCH --mail-user=vmasti@andrew.cmu.edu
+
 
 # Define an array of model names
-names=("1.4b" "2.8b" "6.9b")
+names=("70m" "160m" "410m")
 
 # Loop through each name
 for name in "${names[@]}"
 do
     echo "Running with --name $name"
-    accelerate launch instruction_tuning_raw.py --name $name
+    accelerate launch instruction_tuning_raw.py --name $name --scheduler
 done
